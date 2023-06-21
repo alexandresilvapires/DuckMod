a9c2eca85a05549cc460bf3d790bf723c1e6d106
Alexandre Pires <48489827+alexpires7@users.noreply.github.com>
2023-06-21 23:45:12 +0100

    Add files via upload

---
+++ remappedSrc/net/fabricmc/duckmod/DuckEntityModel.java
@@ -0,0 +1,111 @@
+package net.fabricmc.duckmod;
+
+import com.google.common.collect.ImmutableList;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.model.*;
+import net.minecraft.client.render.entity.model.AnimalModel;
+import net.minecraft.client.render.entity.model.EntityModelPartNames;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
+
+
+@Environment(value=EnvType.CLIENT)
+public class DuckEntityModel<T extends Entity> extends AnimalModel<T> {
+    public static final String RED_THING = "red_thing";
+    private final ModelPart head;
+    private final ModelPart body;
+    private final ModelPart rightLeg;
+    private final ModelPart leftLeg;
+    private final ModelPart rightWing;
+    private final ModelPart leftWing;
+    private final ModelPart beak;
+    private final ModelPart wattle;
+
+    private float head_original;
+    private float torso_original;
+    private float leftWing_original;
+    private float rightWing_original;
+    private float beak_original;
+    private float wattle_original;
+
+    public DuckEntityModel(ModelPart root) {
+        this.head = root.getChild(EntityModelPartNames.HEAD);
+        this.beak = root.getChild(EntityModelPartNames.BEAK);
+        this.wattle = root.getChild(RED_THING);
+        this.body = root.getChild(EntityModelPartNames.BODY);
+        this.rightLeg = root.getChild(EntityModelPartNames.RIGHT_LEG);
+        this.leftLeg = root.getChild(EntityModelPartNames.LEFT_LEG);
+        this.rightWing = root.getChild(EntityModelPartNames.RIGHT_WING);
+        this.leftWing = root.getChild(EntityModelPartNames.LEFT_WING);
+
+        head_original = this.head.pivotY;
+        torso_original = this.body.pivotY;
+        leftWing_original = this.leftWing.pivotY;
+        rightWing_original = this.rightWing.pivotY;
+        beak_original = this.beak.pivotY;
+        wattle_original = this.wattle.pivotY;
+    }
+
+    public static TexturedModelData getTexturedModelData() {
+        ModelData modelData = new ModelData();
+        ModelPartData modelPartData = modelData.getRoot();
+        int i = 16;
+        modelPartData.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create().uv(0, 0).cuboid(-2.0f, -6.0f, -2.0f, 4.0f, 6.0f, 3.0f), ModelTransform.pivot(0.0f, 15.0f, -4.0f));
+        modelPartData.addChild(EntityModelPartNames.BEAK, ModelPartBuilder.create().uv(14, 0).cuboid(-2.0f, -4.0f, -4.0f, 4.0f, 2.0f, 2.0f), ModelTransform.pivot(0.0f, 15.0f, -4.0f));
+        modelPartData.addChild(RED_THING, ModelPartBuilder.create().uv(14, 4).cuboid(-1.0f, -2.0f, -3.0f, 2.0f, 2.0f, 2.0f), ModelTransform.pivot(0.0f, 15.0f, -4.0f));
+        modelPartData.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(0, 9).cuboid(-3.0f, -4.0f, -3.0f, 6.0f, 8.0f, 6.0f), ModelTransform.of(0.0f, 16.0f, 0.0f, 1.5707964f, 0.0f, 0.0f));
+        ModelPartBuilder modelPartBuilder = ModelPartBuilder.create().uv(26, 0).cuboid(-1.0f, 0.0f, -3.0f, 3.0f, 5.0f, 3.0f);
+        modelPartData.addChild(EntityModelPartNames.RIGHT_LEG, modelPartBuilder, ModelTransform.pivot(-2.0f, 19.0f, 1.0f));
+        modelPartData.addChild(EntityModelPartNames.LEFT_LEG, modelPartBuilder, ModelTransform.pivot(1.0f, 19.0f, 1.0f));
+        modelPartData.addChild(EntityModelPartNames.RIGHT_WING, ModelPartBuilder.create().uv(24, 13).cuboid(0.0f, 0.0f, -3.0f, 1.0f, 4.0f, 6.0f), ModelTransform.pivot(-4.0f, 13.0f, 0.0f));
+        modelPartData.addChild(EntityModelPartNames.LEFT_WING, ModelPartBuilder.create().uv(24, 13).cuboid(-1.0f, 0.0f, -3.0f, 1.0f, 4.0f, 6.0f), ModelTransform.pivot(4.0f, 13.0f, 0.0f));
+        return TexturedModelData.of(modelData, 64, 32);
+    }
+
+    @Override
+    protected Iterable<ModelPart> getHeadParts() {
+        return ImmutableList.of(this.head, this.beak, this.wattle);
+    }
+
+    @Override
+    protected Iterable<ModelPart> getBodyParts() {
+        return ImmutableList.of(this.body, this.rightLeg, this.leftLeg, this.rightWing, this.leftWing);
+    }
+
+    @Override
+    public void setAngles(T entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
+        this.head.pitch = headPitch * ((float)Math.PI / 180);
+        this.head.yaw = headYaw * ((float)Math.PI / 180);
+        this.beak.pitch = this.head.pitch;
+        this.beak.yaw = this.head.yaw;
+        this.wattle.pitch = this.head.pitch;
+        this.wattle.yaw = this.head.yaw;
+        this.rightLeg.pitch = MathHelper.cos(limbAngle * 0.6662f) * 1.4f * limbDistance;
+        this.leftLeg.pitch = MathHelper.cos(limbAngle * 0.6662f + (float)Math.PI) * 1.4f * limbDistance;
+        this.rightWing.roll = animationProgress;
+        this.leftWing.roll = -animationProgress;
+
+        DuckEntity duck = (DuckEntity) entity;
+        if (duck.getParking()) {
+            this.body.pivotY = 20;
+            this.rightWing.pivotY = 18;
+            this.leftWing.pivotY = 18;
+            this.head.pivotY = 19;
+            this.beak.pivotY = 19;
+            this.wattle.pivotY = 19;
+            this.rightWing.roll = 0.2f;
+            this.leftWing.roll = -0.2f;
+        }
+        else{
+            this.body.pivotY = torso_original;
+            this.rightWing.pivotY = rightWing_original;
+            this.leftWing.pivotY = leftWing_original;
+            this.head.pivotY = head_original;
+            this.beak.pivotY = beak_original;
+            this.wattle.pivotY = wattle_original;
+        }
+    }
+
+}
---
+++ remappedSrc/net/fabricmc/duckmod/mixin/DuckModMixin.java
@@ -0,0 +1,23 @@
+package net.fabricmc.duckmod.mixin;
+
+import net.fabricmc.duckmod.DuckEntity;
+import net.fabricmc.duckmod.DuckMod;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.feature.DefaultBiomeFeatures;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DefaultBiomeFeatures.class)
+public class DuckModMixin{
+	private static final EntityType<DuckEntity> DUCK = DuckMod.DUCK;
+			
+	@Inject(method = "addFarmAnimals(Lnet/minecraft/world/biome/SpawnSettings$Builder;)V", at = @At("HEAD"), cancellable = true)
+	private static void addFarmAnimals(net.minecraft.world.biome.SpawnSettings.Builder builder, CallbackInfo info) {
+		builder.spawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(DUCK, 10, 4, 4));
+	}
+}
